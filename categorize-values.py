0c41ded2e5f9147558f51593faedf44e7ef99255
Mwanza <victormwanza30@gmail.com>
2024-10-22 17:24:55 +0300

    Added some files

---
+++ categorize-values.py
@@ -0,0 +1,4 @@
+myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+
+for all in myMixedTypeList:
+    print("{} is of the data type {}".format(all,type(all)))
---
+++ collections.py
@@ -0,0 +1,36 @@
+#Defining a list
+
+myFruitList = ["apple", "banana", "cherry"]
+print((myFruitList))
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+
+#Introducing the Tuple data type
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+
+#DICTIONARIES
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
